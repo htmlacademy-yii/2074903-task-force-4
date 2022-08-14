c6ddcfdd46ad98332b1536e324f863a18da46c38
Olga Marinina <youarewonderful@yandex.ru>
2022-08-14 22:28:42 +0700

    Добавила проверку работы класса

---
+++ tests/check-status-indicator.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once '../classes/TaskStatusAndActionsIndicator.php';
+
+$idClient = 1;
+$idExecutor = 5;
+$currentStatus = 'new';
+$currentAction = 'respond';
+
+$TaskOneStatusAndActionsIndicator = new TaskStatusAndActionsIndicator(
+    $idClient, $idExecutor, $currentStatus, $currentAction
+);
+$nextStatus = $TaskOneStatusAndActionsIndicator->getNewStatus();
+
+var_dump($TaskOneStatusAndActionsIndicator->getMapStutesAndActions());
+var_dump(assert($nextStatus === TaskStatusAndActionsIndicator::STATUS_IN_WORK));
+var_dump($TaskOneStatusAndActionsIndicator->getAvailableActions());
