faaef17d92a81fb8355321429c9d4f8206aad636
Olga Marinina <youarewonderful@yandex.ru>
2022-08-22 22:12:54 +0700

    Написала абстрактный класс для действий

---
+++ src/domain/actions/AbstractAction.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+
+namespace omarinina\domain\actions;
+
+class AbstractAction
+{
+    private $action;
+    private $nameAction;
+    private $idClient;
+    private $idExecutor;
+    private $accessUser;
+
+    public function __construct(int $idClient, int $idExecutor)
+    {
+        $this->idClient = $idClient;
+        $this->idExecutor = $idExecutor;
+    }
+
+    public function getAction(): string
+    {
+        return $this->action;
+    }
+
+    public function getNameAction(): string
+    {
+        return $this->nameAction;
+    }
+
+    public function isValidUser($currentUser): bool
+    {
+        return $currentUser === $this->defineAccess() ?  true : false;
+    }
+
+    private function defineAccess(): int
+    {
+        return $this->accessUser;
+    }
+}
