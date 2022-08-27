3b1d3b700b47af2e05f52cb0529414b7cfac6529
Olga Marinina <youarewonderful@yandex.ru>
2022-08-27 21:30:00 +0700

    Поправила схему БД после консультации

--- sql/schema.sql
+++ sql/schema.sql
@@ -7,9 +7,7 @@ CREATE DATABASE taskForce
 USE taskForce;
 
 CREATE TABLE users (
-  /* How I catched we shouldn't use auto increment and we should create ID
-  before inserting any data to the table. Right? */
-  id INT NOT NULL /*AUTO_INCREMENT*/ PRIMARY KEY,
+  uuid INT NOT NULL PRIMARY KEY,
   dt_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   email VARCHAR(128) NOT NULL UNIQUE,
   name VARCHAR(255) NOT NULL,
@@ -22,63 +20,63 @@ CREATE TABLE users (
 CREATE TABLE executorProfiles (
   executor_id INT NOT NULL PRIMARY KEY,
   FOREIGN KEY (executor_id) REFERENCES users(id),
-  avatar VARCHAR(255) DEFAULT NULL,
-  birth_dt TIMESTAMP DEFAULT NULL,
-  phone CHAR(255) DEFAULT NULL,
-  telegram VARCHAR(255) DEFAULT NULL,
-  bio TEXT DEFAULT NULL,
+  avatar VARCHAR(255),
+  birth_dt TIMESTAMP,
+  phone CHAR(255),
+  telegram VARCHAR(255),
+  bio TEXT,
   executor_status VARCHAR(255)
 );
 
 CREATE TABLE categories (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255)/*,
-  eng_name VARCHAR(255)*/
+  name VARCHAR(255)
 );
 
 CREATE TABLE executorCategories (
-  PRIMARY KEY (category_id, executor_id),
-  category_id INT,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  category_id INT NOT NULL,
   FOREIGN KEY (category_id) REFERENCES categories(id),
-  executor_id INT,
+  executor_id INT NOT NULL,
   FOREIGN KEY (executor_id) REFERENCES users(id)
 );
 
 CREATE TABLE tasks (
-  id INT NOT NULL /*AUTO_INCREMENT*/ PRIMARY KEY,
+  uuid INT NOT NULL PRIMARY KEY,
   dt_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   name VARCHAR(255) NOT NULL,
   description TEXT,
   expiry_date TIMESTAMP NOT NULL,
   budget INT NOT NULL,
-  client_id INT,
+  client_id INT NOT NULL,
   FOREIGN KEY (client_id) REFERENCES users(id),
   executor_id INT,
   FOREIGN KEY (executor_id) REFERENCES users(id),
-  category_id INT,
+  category_id INT NOT NULL,
   FOREIGN KEY (category_id) REFERENCES categories(id),
   coordinate POINT NOT NULL,
   task_status VARCHAR(255) NOT NULL
 );
 
+--I'm not sure we need there diff column or not (for example, internal_name).
 CREATE TABLE files (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255)
+  uuid INT NOT NULL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE taskFiles (
-  PRIMARY KEY (file_id, task_id),
-  file_id INT,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  file_id INT NOT NULL,
   FOREIGN KEY (file_id) REFERENCES files(id),
-  task_id INT,
+  task_id INT NOT NULL,
   FOREIGN KEY (task_id) REFERENCES tasks(id)
 );
 
 CREATE TABLE responds (
-  PRIMARY KEY (task_id, executor_id),
-  task_id INT,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  task_id INT NOT NULL,
   FOREIGN KEY (task_id) REFERENCES tasks(id),
-  executor_id INT,
+  executor_id INT NOT NULL,
   FOREIGN KEY (executor_id) REFERENCES users(id),
   dt_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   price INT NOT NULL,
@@ -86,14 +84,15 @@ CREATE TABLE responds (
 );
 
 CREATE TABLE reviews (
-  PRIMARY KEY (task_id, executor_id),
-  task_id INT,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  task_id INT NOT NULL,
   FOREIGN KEY (task_id) REFERENCES tasks(id),
-  executor_id INT,
-  FOREIGN KEY (executor_id) REFERENCES users(id),
-  client_id INT,
-  FOREIGN KEY (client_id) REFERENCES users(id),
-  score INT,
+  -- We can received all this information by task_id, shouldn't we use it here?
+  -- executor_id INT NOT NULL,
+  -- FOREIGN KEY (executor_id) REFERENCES users(id),
+  -- client_id INT,
+  -- FOREIGN KEY (client_id) REFERENCES users(id),
+  score INT NOT NULL,
   comment TEXT,
   dt_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   success BOOLEAN
