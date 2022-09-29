894b025a4458c91e4cac8ae800e71273b178b1be
Olga Marinina <youarewonderful@yandex.ru>
2022-09-29 22:31:11 +0700

    Добавила миграцию на добавлении таблицы roles

---
+++ migrations/m220929_152739_add_data_to_roles.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m220929_152739_add_data_to_roles
+ */
+class m220929_152739_add_data_to_roles extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->batchInsert(
+            'roles',
+            ['role'],
+            [
+                ['client'],
+                ['executor']
+            ]
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->delete('roles');
+    }
+
+}
