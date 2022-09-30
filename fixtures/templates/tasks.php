a56ac1ec96ea41fca8cfdfd837508671f553aa51
Olga Marinina <youarewonderful@yandex.ru>
2022-09-30 10:35:19 +0700

    Сделала шаблон для таблицы задач, класс фикстур, заполнила таблицу

--- fixtures/templates/tasks.php
+++ fixtures/templates/tasks.php
@@ -1,18 +1,31 @@
 <?php
 
-use omarinina\domain\valueObjects\UniqueIdentification;
+use omarinina\domain\models\user\Roles;
+
+$executors = array_map(
+    function ($users) { return $users->id; },
+    Roles::findOne(['role' => 'executor'])->users);
+$clients = array_map(
+    function ($users) { return $users->id; },
+    Roles::findOne(['role' => 'client'])->users);
 
 /**
  * @var $faker \Faker\Generator
  */
 return [
-    'uuid' => UniqueIdentification::createInst()->getId(),
     'name' => $faker->words(3, true),
     'description' => $faker->text(),
-    'expiryDate' => $faker->dateTimeBetween('now', '+2 months'),
+    'expiryDate' => date(
+        'Y-m-d H:i:s',
+        Yii::$app->formatter->asTimestamp(
+            $faker->dateTimeBetween('now', '+2 months')
+        )
+    ),
     'budget' => $faker->randomNumber(4,true),
-    'categoryId' => $faker->randomDigitNot(9),
+    'categoryId' => $faker->randomElement([1, 2, 3, 4, 5, 6, 7, 8]),
     'lat' => $faker->randomFloat(7,41,81),
     'lng' => $faker->randomFloat(7, 19,169),
-    'status' => $faker->randomElements([1, 2, 3, 4, 5])
+    'status' => $faker->randomElement([1, 2, 3, 4, 5]),
+    'executorId' => $faker->randomElement([$faker->randomElement($executors), null]),
+    'clientId' => $faker->randomElement($clients)
 ];
