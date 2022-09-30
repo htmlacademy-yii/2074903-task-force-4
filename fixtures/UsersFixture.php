100b75c439f51ff95ba1715dbe42d67c0ef8b75d
Olga Marinina <youarewonderful@yandex.ru>
2022-09-30 10:01:38 +0700

    Сделала шаблон для таблицы пользователей, класс фикстур и загрузила в таблицу

---
+++ fixtures/UsersFixture.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\fixtures;
+
+use yii\test\ActiveFixture;
+
+class UsersFixture extends ActiveFixture
+{
+    public $modelClass = 'omarinina\domain\models\user\Users';
+
+}
--- fixtures/templates/users.php
+++ fixtures/templates/users.php
@@ -6,10 +6,22 @@ use omarinina\domain\valueObjects\UniqueIdentification;
  * @var $faker \Faker\Generator
  */
 return [
-    'uuid' => UniqueIdentification::createInst()->getId(),
     'email' => $faker->unique()->email(),
     'name' => $faker->userName(),
     'password' => $faker->password(),
-    'role' => $faker->boolean(),
-    'city' => $faker->numberBetween(1,1087)
+    'role' => $faker->randomElement([1, 2]),
+    'city' => $faker->numberBetween(1,1087),
+    'avatarSrc' => $faker->randomElement(
+        [
+            '@app/web/img/avatars/1.png',
+            '@app/web/img/avatars/2.png',
+            '@app/web/img/avatars/3.png',
+            '@app/web/img/avatars/4.png',
+            '@app/web/img/avatars/5.png',
+        ]
+    ),
+    'birthDate' => $faker->date('Y-m-d','-18 years'),
+    'phone' => $faker->phoneNumber(),
+    'telegram' => $faker->lexify('@???????'),
+    'bio' => $faker->text()
 ];
