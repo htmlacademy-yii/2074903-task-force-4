1a683a593626578b284cf443f57cabcbdf007a79
Olga Marinina <youarewonderful@yandex.ru>
2022-09-30 23:06:44 +0700

    Поправила фикстуры в тестах

--- tests/fixtures/data/users.php
+++ tests/fixtures/data/users.php
@@ -1,31 +1,40 @@
 <?php
 
-use omarinina\domain\valueObjects\UniqueIdentification;
-
-
 return [
-    'user1' => [
-        'uuid' => UniqueIdentification::createInst()->getId(),
-        'email' => 'hello@ya.ru',
-        'name' => 'Mr Duck',
-        'password' => 'top-secret',
+    'users0' => [
+        'email' => 'oran89@gmail.com',
+        'name' => 'zparker',
+        'password' => 'Y`-e6I]I',
         'role' => 2,
-        'city' => 2
+        'city' => 2,
+        'avatarSrc' => '@app/web/img/avatars/3.png',
+        'birthDate' => '1976-06-23',
+        'phone' => '1-616-630-3514',
+        'telegram' => '@gtlfzlr',
+        'bio' => 'Dolor eum et iure culpa quaerat quas. Est vero quam ut neque. Reiciendis aut provident quam. Temporibus quia quia optio.',
     ],
-    'user2' => [
-        'uuid' => UniqueIdentification::createInst()->getId(),
-        'email' => 'bye@ya.ru',
-        'name' => 'Catty',
-        'password' => 'just-secret',
+    'users1' => [
+        'email' => 'bogisich.arno@muller.info',
+        'name' => 'kaley24',
+        'password' => '{fU2}2}r~~(x',
         'role' => 1,
-        'city' => 2
+        'city' => 2,
+        'avatarSrc' => '@app/web/img/avatars/5.png',
+        'birthDate' => '2001-11-23',
+        'phone' => '+1 (380) 606-4337',
+        'telegram' => '@jzhdrfx',
+        'bio' => 'Soluta voluptatem minus et nemo inventore. Fugit modi neque velit quo officia consequuntur. Consequatur eveniet fugit placeat deleniti.',
     ],
-    'user3' => [
-        'uuid' => UniqueIdentification::createInst()->getId(),
-        'email' => 'wtf@ya.ru',
-        'name' => 'Strange',
-        'password' => 'secret',
+    'users2' => [
+        'email' => 'providenci56@yahoo.com',
+        'name' => 'bhuels',
+        'password' => 'Ruk,33.2/N',
         'role' => 2,
-        'city' => 1
+        'city' => 1,
+        'avatarSrc' => '@app/web/img/avatars/1.png',
+        'birthDate' => '1988-10-16',
+        'phone' => '435-262-6636',
+        'telegram' => '@philckj',
+        'bio' => 'Vitae ipsum perferendis est illum. Fugit molestias reiciendis odit et voluptatum.',
     ]
 ];
