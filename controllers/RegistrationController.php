332953e03deac7919fc0759f069eb93f6bea25cd
Olga Marinina <youarewonderful@yandex.ru>
2022-10-08 13:31:52 +0700

    Убрала логику записи нового пользователя из модели формы регистрации

--- controllers/RegistrationController.php
+++ controllers/RegistrationController.php
@@ -1,29 +1,37 @@
 <?php
 namespace app\controllers;
 
+use omarinina\domain\models\user\Users;
 use omarinina\infrastructure\models\form\RegistrationForm;
 use omarinina\domain\models\Cities;
 use Yii;
+use yii\base\Exception;
 use yii\web\Controller;
+use yii\web\Response;
 
 class RegistrationController extends Controller
 {
 
     /**
-     * @return string
-     * @throws \yii\base\Exception
+     * @return string|Response
+     * @throws Exception
      */
-    public function actionIndex(): string
+    public function actionIndex(): string|Response
     {
         $registrationForm = new RegistrationForm();
         $cities = Cities::find()->all();
+        $newUser = new Users();
 
         if (Yii::$app->request->getIsPost()) {
             $registrationForm->load(Yii::$app->request->post());
 
             if ($registrationForm->validate()) {
-                $registrationForm->createNewUser();
-                $this->goHome();
+                $newUser->attributes = $_POST['RegistrationForm'];
+                $newUser->password = Yii::$app->getSecurity()->generatePasswordHash($registrationForm->password);
+                $newUser->role =  ($registrationForm->executor === true) ? 2 : 1;
+                $newUser->save(false);
+
+                return $this->goHome();
             }
         }
 
