85e7751af1bf07ce4233660285af2f495076780d
Olga Marinina <youarewonderful@yandex.ru>
2022-10-11 10:26:53 +0700

    Почистила SiteComtroller от ненужных действий, удалила встроенные представления

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -9,7 +9,6 @@ use yii\web\Response;
 use yii\filters\VerbFilter;
 use omarinina\infrastructure\models\form\LoginForm;
 use yii\web\NotFoundHttpException;
-use app\models\ContactForm;
 
 class SiteController extends Controller
 {
@@ -110,31 +109,4 @@ class SiteController extends Controller
         return $this->redirect(['index']);
     }
 
-    /**
-     * Displays contact page.
-     *
-     * @return Response|string
-     */
-    public function actionContact()
-    {
-        $model = new ContactForm();
-        if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['adminEmail'])) {
-            Yii::$app->session->setFlash('contactFormSubmitted');
-
-            return $this->refresh();
-        }
-        return $this->render('contact', [
-            'model' => $model,
-        ]);
-    }
-
-    /**
-     * Displays about page.
-     *
-     * @return string
-     */
-    public function actionAbout()
-    {
-        return $this->render('about');
-    }
 }
