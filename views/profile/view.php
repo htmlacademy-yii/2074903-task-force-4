5d8fe6df78f8403bb6b75bed8c651bc9b4287f22
Olga Marinina <youarewonderful@yandex.ru>
2022-11-02 22:42:41 +0700

    Поправила форму показа профиля пользователя в зависимости от пользователя и существующих данных

--- views/profile/view.php
+++ views/profile/view.php
@@ -5,6 +5,8 @@
 use omarinina\domain\models\user\Users;
 use yii\helpers\Url;
 use Yii;
+use omarinina\infrastructure\constants\UserRoleConstants;
+
 ?>
 <div class="main-content container">
     <div class="left-column">
@@ -14,6 +16,7 @@ use Yii;
                 <img class="card-photo"
                      src="<?= $currentUser->avatarSrc ?>"
                      width="191" height="190" alt="Фото пользователя">
+                <?php if ($currentUser->role === UserRoleConstants::ID_EXECUTOR_ROLE) : ?>
                 <div class="card-rate">
                     <div class="stars-rating big">
                         <?= str_repeat(
@@ -27,35 +30,51 @@ use Yii;
                     </div>
                     <span class="current-rate"><?= $currentUser->getExecutorRating() ?></span>
                 </div>
+                <?php endif; ?>
             </div>
             <p class="user-description">
-                <?= $currentUser->bio ?>
+                <?php if ($currentUser->bio) : ?>
+                    <?= $currentUser->bio ?>
+                <?php else : ?>
+                    Здесь пока ничего нет
+                <?php endif; ?>
             </p>
         </div>
         <div class="specialization-bio">
+            <?php if ($currentUser->role === UserRoleConstants::ID_EXECUTOR_ROLE) : ?>
             <div class="specialization">
-                <p class="head-info">Специализации</p>
+                <p class="head-info">Специализации </p>
                 <ul class="special-list">
-                    <?php foreach ($currentUser->executorCategories as $category) : ?>
-                    <li class="special-item">
-                        <a href="#" class="link link--regular"><?= $category->name ?></a>
-                    </li>
-                    <?php endforeach; ?>
+                    <?php if ($currentUser->executorCategories) : ?>
+                        <?php foreach ($currentUser->executorCategories as $category) : ?>
+                            <li class="special-item">
+                                <a href="#" class="link link--regular"><?= $category->name ?></a>
+                            </li>
+                        <?php endforeach; ?>
+                    <?php else : ?>
+                        <li class="special-item">
+                            <a href="#" class="link link--regular">Категории не выбраны</a>
+                        </li>
+                    <?php endif; ?>
                 </ul>
             </div>
+            <?php endif; ?>
             <div class="bio">
                 <p class="head-info">Био</p>
                 <p class="bio-info"><span class="country-info">Россия</span>,
-                    <span class="town-info"><?= $currentUser->userCity->name ?></span>,
-                    <span class="age-info"><?= \morphos\Russian\pluralize(
-                        round(Yii::$app->formatter->asTimestamp($currentUser->birthDate) / (365 * 24 * 60 * 60)),
-                        'год'
-                    ) ?></span>
+                    <span class="town-info"><?= $currentUser->userCity->name ?></span>
+                    <?php if ($currentUser->birthDate) : ?>
+                        <span class="age-info">, <?= \morphos\Russian\pluralize(
+                            round(Yii::$app->formatter->asTimestamp($currentUser->birthDate) / (365 * 24 * 60 * 60)),
+                            'год'
+                            ) ?></span>
+                    <?php endif; ?>
                 </p>
             </div>
         </div>
+        <?php if ($currentUser->executorReviews) : ?>
         <h4 class="head-regular">Отзывы заказчиков</h4>
-        <?php foreach ($currentUser->executorReviews as $executorReview) : ?>
+            <?php foreach ($currentUser->executorReviews as $executorReview) : ?>
         <div class="response-card">
             <img class="customer-photo"
                  src="<?= $executorReview->client->avatarSrc ?>"
@@ -84,10 +103,12 @@ use Yii;
                     </span> назад</p>
             </div>
         </div>
-        <?php endforeach; ?>
+            <?php endforeach; ?>
+        <?php endif; ?>
 
     </div>
     <div class="right-column">
+        <?php if ($currentUser->role === UserRoleConstants::ID_EXECUTOR_ROLE) : ?>
         <div class="right-card black">
             <h4 class="head-card">Статистика исполнителя</h4>
             <dl class="black-list">
@@ -102,19 +123,28 @@ use Yii;
                 <dd><?= $currentUser->getExecutorCurrentStatus() ?></dd>
             </dl>
         </div>
+        <?php endif; ?>
+        <?php if (!$currentUser->hidden) : ?>
         <div class="right-card white">
             <h4 class="head-card">Контакты</h4>
             <ul class="enumeration-list">
+                <?php if ($currentUser->phone) : ?>
                 <li class="enumeration-item">
                     <a href="#" class="link link--block link--phone"><?= $currentUser->phone ?></a>
                 </li>
+                <?php endif; ?>
+                <?php if ($currentUser->email) : ?>
                 <li class="enumeration-item">
                     <a href="#" class="link link--block link--email"><?= $currentUser->email ?></a>
                 </li>
+                <?php endif; ?>
+                <?php if ($currentUser->telegram) : ?>
                 <li class="enumeration-item">
                     <a href="#" class="link link--block link--tg"><?= $currentUser->telegram ?></a>
                 </li>
+                <?php endif; ?>
             </ul>
         </div>
+        <?php endif; ?>
     </div>
 </div>
